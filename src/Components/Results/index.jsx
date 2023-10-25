466d19f03765604302f0f13ffd504acb643d5afc
Quasark <q@quasark.xyz>
2023-10-25 16:53:09 -0700

    lab complete

--- src/Components/Results/index.jsx
+++ src/Components/Results/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import JSONPretty from 'react-json-pretty';
 import 'react-json-pretty/themes/monikai.css';
+import './Results.scss';
 
 // class Results extends React.Component {
 const Results = (props) => {
   return (
-    <section>
-      {/* <pre>{props.data ? JSON.stringify(props.data, undefined, 2) : null}</pre> */}
+    <div className='results'>
       <JSONPretty data-testid='results' id="json-pretty" data={props} ></JSONPretty>
-    </section>
+    </div>
   );
 }
 
